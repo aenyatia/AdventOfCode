8db3290afc7194c1a305c0874d03de8386587848
Aenyatia <aenyatia@gmail.com>
2022-12-01 19:42:08 +0100

    2021 - Day1 - Part1

---
+++ AdventOfCode/2021/Day1/Day1.cs
@@ -0,0 +1,20 @@
+﻿namespace AdventOfCode._2021.Day1;
+
+public class Day1
+{
+    public static void Part1()
+    {
+        var input = File
+            .ReadAllLines("2021/Day1/input.txt")
+            .Select(int.Parse)
+            .ToList();
+
+        var count = 0;
+
+        for (var i = 1; i < input.Count; i++)
+            if (input[i] > input[i - 1])
+                count++;
+
+        Console.WriteLine(count);
+    }
+}
