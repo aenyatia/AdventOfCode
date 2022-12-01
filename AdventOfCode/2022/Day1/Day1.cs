852961c79af309dd3b968fd010a404f86f72529b
Aenyatia <aenyatia@gmail.com>
2022-12-01 19:32:33 +0100

    2022 - Day1 - Part1

---
+++ AdventOfCode/2022/Day1/Day1.cs
@@ -0,0 +1,29 @@
+﻿namespace AdventOfCode._2022.Day1;
+
+public class Day1
+{
+    public static void Part1()
+    {
+        var input = File
+            .ReadAllLines("2022/Day1/input.txt")
+            .Select(line => int.TryParse(line, out var n) ? n : (int?)null);
+
+        var sum = 0;
+        var max = 0;
+        foreach (var n in input)
+        {
+            if (n.HasValue)
+            {
+                sum += n.Value;
+                continue;
+            }
+
+            if (sum > max)
+                max = sum;
+
+            sum = 0;
+        }
+
+        Console.WriteLine(max);
+    }
+}
