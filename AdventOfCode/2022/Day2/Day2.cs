43317e30922ccb8b9b976b953a4fa11edd068a97
Aenyatia <aenyatia@gmail.com>
2022-12-02 14:32:41 +0100

    2022 - Day2 - Part1

---
+++ AdventOfCode/2022/Day2/Day2.cs
@@ -0,0 +1,33 @@
+﻿namespace AdventOfCode._2022.Day2;
+
+public class Day2
+{
+    public static void Part1()
+    {
+        var score = File.ReadAllLines("2022/Day2/input.txt")
+            .Select(line => line.Split(" "))
+            .Sum(GetScore);
+
+        Console.WriteLine(score);
+    }
+
+    private static int GetScore(IReadOnlyList<string> args)
+    {
+        return (args[0], args[1]) switch
+        {
+            ("A", "X") => 1 + 3,
+            ("A", "Y") => 2 + 6,
+            ("A", "Z") => 3 + 0,
+
+            ("B", "X") => 1 + 0,
+            ("B", "Y") => 2 + 3,
+            ("B", "Z") => 3 + 6,
+
+            ("C", "X") => 1 + 6,
+            ("C", "Y") => 2 + 0,
+            ("C", "Z") => 3 + 3,
+
+            _ => throw new ArgumentOutOfRangeException()
+        };
+    }
+}
