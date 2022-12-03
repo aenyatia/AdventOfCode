acd62fb7c1004877fd5ea7dadecaa86cba9ecb7b
Aenyatia <aenyatia@gmail.com>
2022-12-03 20:32:38 +0100

    2021 - Day2 - Part2

--- AdventOfCode/2021/Day2/Day2.cs
+++ AdventOfCode/2021/Day2/Day2.cs
@@ -30,4 +30,35 @@ public class Day2
 
         Console.WriteLine(position * depth);
     }
+
+    public static void Part2()
+    {
+        var commands = File
+            .ReadAllLines("2021/Day2/input.txt")
+            .Select(line => line.Split())
+            .Select(cmd => (cmd[0], int.Parse(cmd[1])));
+
+        var position = 0;
+        var depth = 0;
+        var aim = 0;
+
+        foreach (var (command, value) in commands)
+        {
+            switch (command)
+            {
+                case "forward":
+                    position += value;
+                    depth += aim * value;
+                    break;
+                case "down":
+                    aim += value;
+                    break;
+                case "up":
+                    aim -= value;
+                    break;
+            }
+        }
+
+        Console.WriteLine(position * depth);
+    }
 }
