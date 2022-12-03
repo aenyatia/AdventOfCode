6f223f0236bfa2b7cfc68ed7929decc1c1a333b6
Aenyatia <aenyatia@gmail.com>
2022-12-03 08:23:12 +0100

    2022 - Day3 - Part1

---
+++ AdventOfCode/2022/Day3/Day3.cs
@@ -0,0 +1,30 @@
+﻿namespace AdventOfCode._2022.Day3;
+
+public class Day3
+{
+    public static void Part1()
+    {
+        var input = File.ReadAllLines("2022/Day3/input.txt");
+
+        var itemTypes = new List<char>();
+
+        foreach (var line in input)
+        {
+            var set = new HashSet<char>();
+
+            for (var i = 0; i < line.Length / 2; i++)
+                set.Add(line[i]);
+
+            for (var i = line.Length / 2; i < line.Length; i++)
+                if (set.Contains(line[i]))
+                {
+                    itemTypes.Add(line[i]);
+                    break;
+                }
+        }
+
+        var sum = itemTypes.Sum(i => char.IsLower(i) ? i - 96 : i - 38);
+
+        Console.WriteLine(sum);
+    }
+}
