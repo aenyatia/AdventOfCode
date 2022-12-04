bfd87a3d22b663c7a32b31e163c546bae142f0e9
Aenyatia <aenyatia@gmail.com>
2022-12-04 09:46:24 +0100

    2022 - Day4 - Part2

--- AdventOfCode/2022/Day4/Day4.cs
+++ AdventOfCode/2022/Day4/Day4.cs
@@ -23,4 +23,26 @@ public class Day4
 
         Console.WriteLine(fullyOverlap);
     }
+
+    public static void Part2()
+    {
+        var input = File
+            .ReadAllLines("2022/Day4/input.txt")
+            .Select(line => line.Split('-', ',')
+                .Select(int.Parse)
+                .ToArray())
+            .ToList();
+
+        var notOverlap =
+            (from line in input
+             let beginSectionA = line[0]
+             let endSectionA = line[1]
+             let beginSectionB = line[2]
+             let endSectionB = line[3]
+             where beginSectionB > endSectionA || endSectionB < beginSectionA
+             select line)
+            .Count();
+
+        Console.WriteLine(input.Count - notOverlap);
+    }
 }
