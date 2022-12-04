f37fc91ab4f3d566591c6b4aff947573e890d2bf
Aenyatia <aenyatia@gmail.com>
2022-12-04 09:57:16 +0100

    2022 - Day4 - Part2 - Update

--- AdventOfCode/2022/Day4/Day4.cs
+++ AdventOfCode/2022/Day4/Day4.cs
@@ -30,19 +30,18 @@ public class Day4
             .ReadAllLines("2022/Day4/input.txt")
             .Select(line => line.Split('-', ',')
                 .Select(int.Parse)
-                .ToArray())
-            .ToList();
+                .ToArray());
 
-        var notOverlap =
+        var overlap =
             (from line in input
              let beginSectionA = line[0]
              let endSectionA = line[1]
              let beginSectionB = line[2]
              let endSectionB = line[3]
-             where beginSectionB > endSectionA || endSectionB < beginSectionA
+             where !(beginSectionB > endSectionA || endSectionB < beginSectionA)
              select line)
             .Count();
 
-        Console.WriteLine(input.Count - notOverlap);
+        Console.WriteLine(overlap);
     }
 }
