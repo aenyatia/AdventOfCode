2e9575eeaab4b38becebbfc9e70966d069beb0f9
Aenyatia <aenyatia@gmail.com>
2022-12-05 16:29:20 +0100

    2022 - Day5 - Part2

--- AdventOfCode/2022/Day5/Day5.cs
+++ AdventOfCode/2022/Day5/Day5.cs
@@ -36,4 +36,44 @@ public class Day5
 
         Console.WriteLine(string.Concat(result));
     }
+
+    public static void Part2()
+    {
+        var input = File.ReadAllLines("2022/Day5/input.txt");
+
+        var boxes = input
+            .Where(s => s.StartsWith('['))
+            .Reverse();
+
+        var commands = input
+            .Where(s => s.StartsWith("move"))
+            .Select(s => s.Split(" "))
+            .Select(s => (int.Parse(s[1]), int.Parse(s[3]), int.Parse(s[5])));
+
+        var stacks = Enumerable
+            .Repeat(0, 9)
+            .Select(s => new Stack<char>())
+            .ToList();
+
+        foreach (var box in boxes)
+            for (var i = 1; i < box.Length; i += 4)
+                if (box[i] != ' ')
+                    stacks[i / 4].Push(box[i]);
+
+        foreach (var (count, from, to) in commands)
+        {
+            var stack = new Stack<char>();
+
+            for (var i = 0; i < count; i++)
+                stack.Push(stacks[from - 1].Pop());
+
+            for (var i = 0; i < count; i++)
+                stacks[to - 1].Push(stack.Pop());
+        }
+
+        var result = stacks
+            .Select(s => s.Peek());
+
+        Console.WriteLine(string.Concat(result));
+    }
 }
