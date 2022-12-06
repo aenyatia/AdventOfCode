b1b4f7e2e02913d9fa45038f958bcb88b398bce3
Aenyatia <aenyatia@gmail.com>
2022-12-06 15:05:50 +0100

    2022 - Day6 - Part1

---
+++ AdventOfCode/2022/Day6/Day6.cs
@@ -0,0 +1,24 @@
+﻿namespace AdventOfCode._2022.Day6;
+
+public class Day6
+{
+    public static void Part1()
+    {
+        var input = File
+            .ReadAllText("2022/Day6/input.txt")
+            .Trim();
+
+        var result = -1;
+        for (var i = 4; i < input.Length; i++)
+        {
+            var set = input[(i - 4)..i].ToHashSet();
+
+            if (set.Count != 4) continue;
+
+            result = i;
+            break;
+        }
+
+        Console.WriteLine(result);
+    }
+}
