b7ac8562faebb11845ccb18f7ea3eef7383a6d0e
Aenyatia <aenyatia@gmail.com>
2022-12-09 14:01:32 +0100

    2022 - Day8 - Part1 - Version 2

--- AdventOfCode/2022/Day8/Day8.cs
+++ AdventOfCode/2022/Day8/Day8.cs
@@ -124,4 +124,76 @@ public class Day8
 
         Console.WriteLine(maxVisibleTrees);
     }
+
+    public static void Part1Ver2()
+    {
+        var input = File
+            .ReadAllLines("2022/Day8/input.txt")
+            .Select(line => line
+                .Select(c => (int)char.GetNumericValue(c))
+                .ToList())
+            .ToList();
+
+        var rows = input.Count;
+        var cols = input.First().Count;
+
+        var map = new Dictionary<(int row, int col), Heights>();
+
+        for (var n = 0; n < rows; n++)
+        {
+            for (int left = 0, right = rows - 1; left < rows; left++, right--)
+            {
+                var leftKey = (n, left);
+                var rightKey = (n, right);
+                var upKey = (left, n);
+                var downKey = (right, n);
+
+                if (!map.ContainsKey(leftKey)) map.Add(leftKey, new Heights());
+                if (!map.ContainsKey(rightKey)) map.Add(rightKey, new Heights());
+                if (!map.ContainsKey(upKey)) map.Add(upKey, new Heights());
+                if (!map.ContainsKey(downKey)) map.Add(downKey, new Heights());
+
+                if (left == 0)
+                {
+                    map[leftKey].Left = input[n][left];
+                    map[rightKey].Right = input[n][right];
+                    map[upKey].Up = input[left][n];
+                    map[downKey].Down = input[right][n];
+                    continue;
+                }
+
+                map[leftKey].Left = Math.Max(map[(n, left - 1)].Left, input[n][left]);
+                map[rightKey].Right = Math.Max(map[(n, right + 1)].Right, input[n][right]);
+                map[upKey].Up = Math.Max(map[(left - 1, n)].Up, input[left][n]);
+                map[downKey].Down = Math.Max(map[(right + 1, n)].Down, input[right][n]);
+            }
+        }
+
+        var visibleTrees = 4 * rows - 4;
+        for (var row = 1; row < rows - 1; row++)
+            for (var col = 1; col < cols - 1; col++)
+            {
+                var value = input[row][col];
+                var left = (row, col - 1);
+                var right = (row, col + 1);
+                var up = (row - 1, col);
+                var down = (row + 1, col);
+
+                if (map[left].Left < value ||
+                    map[right].Right < value ||
+                    map[up].Up < value ||
+                    map[down].Down < value)
+                    visibleTrees++;
+            }
+
+        Console.WriteLine(visibleTrees);
+    }
+}
+
+public class Heights
+{
+    public int Left { get; set; }
+    public int Right { get; set; }
+    public int Up { get; set; }
+    public int Down { get; set; }
 }
