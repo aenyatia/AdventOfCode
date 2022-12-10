498e4ff8bde64cd787737d3d2e5887b3381dc8c1
Aenyatia <aenyatia@gmail.com>
2022-12-10 22:57:51 +0100

    2022 - Day10 - Part2

--- AdventOfCode/2022/Day10/Day10.cs
+++ AdventOfCode/2022/Day10/Day10.cs
@@ -8,6 +8,20 @@ public class Day10
 
         Console.WriteLine(new CpuPart1().Execute(instructions));
     }
+
+    public static void Part2()
+    {
+        var instructions = File.ReadAllLines("2022/Day10/input.txt");
+        var screen = new CpuPart2().Execute(instructions).Chunk(40);
+
+        foreach (var line in screen)
+        {
+            foreach (var c in line)
+                Console.Write(c);
+
+            Console.WriteLine();
+        }
+    }
 }
 
 public class CpuPart1
@@ -48,5 +62,49 @@ public class CpuPart1
     {
         if (_interestingCycles.Contains(_cycle))
             _signalStrength += _cycle * _x;
+    }
+}
+
+public class CpuPart2
+{
+    private readonly IList<char> _screen = new List<char>();
+
+    private int _cycle;
+    private int _x = 1;
+    private int _sprite = 1;
+
+    public IEnumerable<char> Execute(IEnumerable<string> instructions)
+    {
+        foreach (var instruction in instructions)
+        {
+            var parts = instruction.Split(" ");
+
+            if (parts[0] == "noop")
+            {
+                _cycle++;
+                UpdateScreen();
+            }
+            else
+            {
+                _cycle++;
+                UpdateScreen();
+
+                _cycle++;
+                UpdateScreen();
+
+                _x += int.Parse(parts[1]);
+                _sprite = _x;
+            }
+        }
+
+        return _screen;
+    }
+
+    private void UpdateScreen()
+    {
+        var pixel = (_cycle - 1) % 40;
+
+        if (_sprite >= pixel - 1 && _sprite <= pixel + 1) _screen.Add('#');
+        else _screen.Add(' ');
     }
 }
