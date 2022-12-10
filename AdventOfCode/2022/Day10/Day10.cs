6e8d20bb65e95cce0d187e6c3eba58dd69edaff0
Aenyatia <aenyatia@gmail.com>
2022-12-10 22:56:48 +0100

    2022 - Day10 - Part1

---
+++ AdventOfCode/2022/Day10/Day10.cs
@@ -0,0 +1,52 @@
+﻿namespace AdventOfCode._2022.Day10;
+
+public class Day10
+{
+    public static void Part1()
+    {
+        var instructions = File.ReadAllLines("2022/Day10/input.txt");
+
+        Console.WriteLine(new CpuPart1().Execute(instructions));
+    }
+}
+
+public class CpuPart1
+{
+    private readonly ISet<int> _interestingCycles = new HashSet<int> { 20, 60, 100, 140, 180, 220 };
+
+    private int _cycle;
+    private int _x = 1;
+    private int _signalStrength;
+
+    public int Execute(IEnumerable<string> instructions)
+    {
+        foreach (var instruction in instructions)
+        {
+            var parts = instruction.Split(" ");
+
+            if (parts[0] == "noop")
+            {
+                _cycle++;
+                UpdateSignalStrength();
+            }
+            else
+            {
+                _cycle++;
+                UpdateSignalStrength();
+
+                _cycle++;
+                UpdateSignalStrength();
+
+                _x += int.Parse(parts[1]);
+            }
+        }
+
+        return _signalStrength;
+    }
+
+    private void UpdateSignalStrength()
+    {
+        if (_interestingCycles.Contains(_cycle))
+            _signalStrength += _cycle * _x;
+    }
+}
