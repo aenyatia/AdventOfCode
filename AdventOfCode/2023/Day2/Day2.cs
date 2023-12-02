208b6169ed12e44df8bef12582d0bf2888f0dcb9
Aenyatia Nirvalen <aenyatia@gmail.com>
2023-12-02 21:25:55 +0100

    2023 - Day2 - Part2

--- AdventOfCode/2023/Day2/Day2.cs
+++ AdventOfCode/2023/Day2/Day2.cs
@@ -50,6 +50,49 @@ public class Day2
 
         Console.WriteLine(gameSum);
     }
+
+    public static void Part2()
+    {
+        var input = File.ReadAllLines("2023/Day2/input.txt")
+            .Select(l => l.Split(':', ';'));
+
+        var games = new List<Game>();
+        foreach (var line in input)
+        {
+            var id = int.Parse(line.First().Remove(0, 5));
+            var redCubes = new List<int>();
+            var greenCubes = new List<int>();
+            var blueCubes = new List<int>();
+
+            foreach (var item in line.Skip(1))
+            foreach (var c in item.Split(','))
+            {
+                var cube = c.Trim().Split(' ');
+
+                switch (cube.Last())
+                {
+                    case "red":
+                        redCubes.Add(int.Parse(cube.First()));
+                        break;
+                    case "green":
+                        greenCubes.Add(int.Parse(cube.First()));
+                        break;
+                    case "blue":
+                        blueCubes.Add(int.Parse(cube.First()));
+                        break;
+                }
+            }
+
+            games.Add(new Game(id, redCubes, greenCubes, blueCubes));
+        }
+
+        var gameSum = games.Sum(game =>
+            (game.RedCubes.Count != 0 ? game.RedCubes.Max() : 1) *
+            (game.GreenCubes.Count != 0 ? game.GreenCubes.Max() : 1) *
+            (game.BlueCubes.Count != 0 ? game.BlueCubes.Max() : 1));
+
+        Console.WriteLine(gameSum);
+    }
 }
 
 public record Game(int Id, List<int> RedCubes, List<int> GreenCubes, List<int> BlueCubes);
