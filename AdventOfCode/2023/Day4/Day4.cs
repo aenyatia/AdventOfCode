8a7dae41dc5857531be4f34c63610083eb74a26f
Aenyatia Nirvalen <aenyatia@gmail.com>
2023-12-04 09:18:54 +0100

    2023 - Day4 - Part2

--- AdventOfCode/2023/Day4/Day4.cs
+++ AdventOfCode/2023/Day4/Day4.cs
@@ -31,4 +31,36 @@ public class Day4
 
         Console.WriteLine(pointsSum);
     }
+
+    public static void Part2()
+    {
+        var input = File.ReadAllLines("2023/Day4/input.txt")
+            .Select(l => l.Split(Separators, StringSplitOptions.RemoveEmptyEntries))
+            .ToList();
+
+        var dictionary = new Dictionary<int, int>();
+        foreach (var line in input)
+        {
+            var card = int.Parse(line[0].Split(' ').Last());
+
+            var winningNumbers = line[1]
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToHashSet();
+
+            var guessedNumbers = line[2]
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .Count(number => winningNumbers.Contains(number));
+
+            for (var key = card; key < card + guessedNumbers + 1; key++)
+                if (!dictionary.ContainsKey(key)) dictionary[key] = 1;
+                else dictionary[key] += 1;
+
+            for (var key = card + 1; key < card + guessedNumbers + 1; key++)
+                dictionary[key] += dictionary[card] - 1;
+        }
+
+        Console.WriteLine(dictionary.Sum(kvp => kvp.Value));
+    }
 }
