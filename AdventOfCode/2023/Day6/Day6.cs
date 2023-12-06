a990d7a89257505d37499fc16b751f67a903966a
Aenyatia Nirvalen <aenyatia@gmail.com>
2023-12-06 11:16:38 +0100

    2023 - Day6 - Part2

--- AdventOfCode/2023/Day6/Day6.cs
+++ AdventOfCode/2023/Day6/Day6.cs
@@ -28,4 +28,29 @@ public class Day6
 
         Console.WriteLine(result);
     }
+
+    public static void Part2()
+    {
+        var input = File.ReadAllLines("2023/Day6/input.txt")
+            .Select(l => l.Split(' ', StringSplitOptions.RemoveEmptyEntries))
+            .Select(l => l.Skip(1).ToList())
+            .ToList();
+
+        var time = long.Parse(string.Join("", input[0]));
+        var distance = long.Parse(string.Join("", input[1]));
+        
+        var count = 0;
+        for (var i = 0; i < time; i++)
+        {
+            if ((time - i) * i <= distance)
+            {
+                count += 1;
+                continue;                
+            }
+            
+            break;
+        }
+
+        Console.WriteLine(time + 1 - 2 * count);
+    }
 }
