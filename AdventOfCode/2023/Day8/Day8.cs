9f1867d4ba11cda700264f246c50d6ac567d93ce
Aenyatia Nirvalen <aenyatia@gmail.com>
2023-12-08 16:03:59 +0100

    2023 - Day8 - Part2

--- AdventOfCode/2023/Day8/Day8.cs
+++ AdventOfCode/2023/Day8/Day8.cs
@@ -26,4 +26,69 @@ public class Day8
 
         Console.WriteLine(steps);
     }
+
+    public static void Part2()
+    {
+        var input = File.ReadAllLines("2023/Day8/input.txt");
+
+        var navigation = input.First();
+        var maps = new Dictionary<string, (string left, string right)>();
+        foreach (var line in input.Skip(2))
+            maps[line[..3]] = (line[7..10], line[12..15]);
+
+        var currents = maps
+            .Where(kvp => kvp.Key.EndsWith('A'))
+            .Select(kvp => kvp.Key)
+            .ToList();
+
+        var steps = currents
+            .Select(StepsToLastZ)
+            .ToList();
+
+        Console.WriteLine(Lcm(steps));
+        return;
+
+        int StepsToLastZ(string node)
+        {
+            var stepsCount = 0;
+            while (node[2] != 'Z')
+            {
+                node = navigation[stepsCount % navigation.Length] == 'L'
+                    ? maps[node].left
+                    : maps[node].right;
+
+                stepsCount += 1;
+            }
+
+            return stepsCount;
+        }
+
+        long Lcm(IList<int> listOfNumbers)
+        {
+            long lcm = 1;
+            var divisor = 2;
+
+            while (true)
+            {
+                var counter = 0;
+                var divisible = false;
+                for (var i = 0; i < listOfNumbers.Count; i++)
+                {
+                    if (listOfNumbers[i] == 1)
+                        counter++;
+
+                    if (listOfNumbers[i] % divisor != 0) continue;
+
+                    divisible = true;
+                    listOfNumbers[i] /= divisor;
+                }
+
+                if (divisible) lcm *= divisor;
+                else divisor++;
+
+                if (counter == listOfNumbers.Count)
+                    return lcm;
+            }
+        }
+    }
 }
