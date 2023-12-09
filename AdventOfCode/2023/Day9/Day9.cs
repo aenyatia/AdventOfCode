94a3f7be5c46be93ab8aae72e6d20a6b80b284af
Aenyatia Nirvalen <aenyatia@gmail.com>
2023-12-09 23:17:32 +0100

    2023 - Day9 - Part1

---
+++ AdventOfCode/2023/Day9/Day9.cs
@@ -0,0 +1,40 @@
+namespace AdventOfCode._2023.Day9;
+
+public class Day9
+{
+    public static void Part1()
+    {
+        var input = File.ReadAllLines("2023/Day9/input.txt")
+            .Select(l => l.Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList())
+            .ToList();
+
+        var resultParts = new List<int>();
+        foreach (var history in input)
+        {
+            var allZeros = false;
+            var repeat = 0;
+
+            while (!allZeros)
+            {
+                allZeros = true;
+
+                for (var i = 0; i < history.Count - 1 - repeat; i++)
+                {
+                    var diff = history[i + 1] - history[i];
+
+                    history[i] = diff;
+
+                    if (diff != 0) allZeros = false;
+                }
+
+                repeat++;
+            }
+
+            resultParts.Add(history.Sum());
+        }
+
+        Console.WriteLine(resultParts.Sum());
+    }
+}
