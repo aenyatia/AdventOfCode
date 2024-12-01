c1b89137e112160e31f94b9a0fd8d0db46dca86a
Aenyatia <aenyatia@gmail.com>
2024-12-01 19:15:31 +0100

    Add new lines to increase readability.

--- AdventOfCode/2024/Day1/Day1.cs
+++ AdventOfCode/2024/Day1/Day1.cs
@@ -7,35 +7,54 @@ public static class Day1
     public static void Part1()
     {
         var input = File.ReadAllLines(Path)
-            .Select(line => line.Split(' ', StringSplitOptions.RemoveEmptyEntries)
-                .Select(int.Parse).ToArray())
+            .Select(line => line
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray())
             .ToArray();
 
-        var leftNumbers = input.Select(pair => pair[0]).Order().ToArray();
-        var rightNumbers = input.Select(pair => pair[1]).Order().ToArray();
+        var leftNumbers = input
+            .Select(pair => pair[0])
+            .Order()
+            .ToArray();
 
-        var totalDistance = leftNumbers.Zip(rightNumbers, (left, right)
-            => Math.Abs(left - right)).Sum();
+        var rightNumbers = input
+            .Select(pair => pair[1])
+            .Order()
+            .ToArray();
 
-        Console.WriteLine($"[Part1] Total distance: {totalDistance}");
+        var totalDistance = leftNumbers
+            .Zip(rightNumbers, (left, right) => Math.Abs(left - right))
+            .Sum();
+
+        Console.WriteLine($"[Part1] {totalDistance}");
     }
 
     public static void Part2()
     {
         var input = File.ReadAllLines(Path)
-            .Select(line => line.Split(' ', StringSplitOptions.RemoveEmptyEntries)
-                .Select(int.Parse).ToArray())
+            .Select(line => line
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray())
             .ToArray();
 
-        var leftNumbers = input.Select(pair => pair[0]).Order().ToArray();
-        var rightNumbers = input.Select(pair => pair[1]).Order().ToArray();
+        var leftNumbers = input
+            .Select(pair => pair[0])
+            .Order()
+            .ToArray();
 
-        var similarityDict = rightNumbers.GroupBy(n => n)
+        var rightNumbers = input
+            .Select(pair => pair[1])
+            .ToArray();
+
+        var dictionary = rightNumbers.GroupBy(number => number)
             .ToDictionary(g => g.Key, g => g.Count());
 
-        var similarityScore = leftNumbers.Where(n => similarityDict.ContainsKey(n))
-            .Sum(n => n * similarityDict[n]);
+        var similarityScore = leftNumbers
+            .Where(leftNumber => dictionary.ContainsKey(leftNumber))
+            .Sum(leftNumber => leftNumber * dictionary[leftNumber]);
 
-        Console.WriteLine($"[Part2] Similarity Score: {similarityScore}");
+        Console.WriteLine($"[Part2] {similarityScore}");
     }
 }
