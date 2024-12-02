9b4877987f14e5d79ccef5bcebc4b78eded60aad
Aenyatia <aenyatia@gmail.com>
2024-12-02 14:21:38 +0100

    2024 - Day2 - Part2 - Version 2

--- AdventOfCode/2024/Day2/Day2.cs
+++ AdventOfCode/2024/Day2/Day2.cs
@@ -30,7 +30,36 @@ public static class Day2
         Console.WriteLine($"[Part2] {safeReports}");
     }
 
+    public static void Part2Ver2()
+    {
+        var input = File.ReadAllLines(Path)
+            .Select(line => line.Split(' ')
+                .Select(int.Parse).ToArray())
+            .ToArray();
+
+        var safeReports = 0;
+        foreach (Span<int> level in input)
+        {
+            for (var i = 0; i < level.Length; i++)
+            {
+                Span<int> subLevel = [..level[..i], ..level[(i + 1)..]];
+
+                if (!IsSafe(subLevel)) continue;
+
+                safeReports++;
+                break;
+            }
+        }
+
+        Console.WriteLine($"[Part2] {safeReports}");
+    }
+
     private static bool IsSafe(int[] level)
+    {
+        return IsSafe(level.AsSpan());
+    }
+
+    private static bool IsSafe(Span<int> level)
     {
         var diff = level[1] - level[0];
 
